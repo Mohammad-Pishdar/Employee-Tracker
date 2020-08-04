14fab210ace98725b5796b6f47883dc248788b4a
Mo Pishdar <mohpish@gmail.com>
2020-08-04 17:01:35 +1000

    The overall employee information table now includes all the needed info about an employee including the manager column

--- app.js
+++ app.js
@@ -87,8 +87,19 @@ appStart = () => {
 const showAllEmployees = () => {
     connection.query("SELECT employee.id, first_name, last_name, title, department, salary FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id", function (err, res) {
         if (err) throw err;
-        console.log(columnify(res));
-        console.log("-----------------------------------");
+        connection.query("SELECT * FROM employee", function (error, response) {
+            if (error) throw error;
+            for (let i = 0; i < response.length; i++) {
+                if (response[i].manager_id === null) {
+                    res[i].manager = "NULL"
+                } else {
+                    res[i].manager = res[response[i].manager_id - 1].first_name + " " + res[response[i].manager_id - 1].last_name;
+                }
+            }
+            console.log(columnify(res));
+            console.log("-----------------------------------");
+        })
+
     });
 }
 
