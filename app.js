6a5b59adc7e27c37073ba79fb6dad865a295f342
Mo Pishdar <mohpish@gmail.com>
2020-08-06 03:15:20 +1000

    show employees by department function has been fully restructured so now it gives the user to choose what department they want to see the employyes in and gives back a list of employees for that department

--- app.js
+++ app.js
@@ -119,24 +119,35 @@ const showAllEmployees = () => {
 }
 
 const showAllEmployeesByDepartment = () => {
-    connection.query("select first_name, last_name, department from employee inner join role on employee.role_id = role.id inner join department on role.department_id = department.id ", function (err, res) {
-        if (err) throw err;
-        connection.query("SELECT * FROM employee", function (error, response) {
-            if (error) throw error;
+    inquirer
+        .prompt({
+            name: "department",
+            type: "list",
+            message: "Choose the deoartment you want to see the employees in",
+            choices: [
+                "Sales",
+                "Engineering",
+                "Finance",
+                "Legal"
+            ]
+        }).then(answer => {
+            connection.query("select first_name, last_name, department from employee inner join role on employee.role_id = role.id inner join department on role.department_id = department.id where department = " + "'" + answer.department + "'", function (err, res) {
+                if (err) throw err;
 
-            console.log(columnify(res, {
-                minWidth: 15,
-                config: {
-                    id: {
-                        maxWidth: 3
+                console.log(columnify(res, {
+                    minWidth: 15,
+                    config: {
+                        id: {
+                            maxWidth: 3
+                        }
                     }
-                }
-            }));
-            console.log("-----------------------------------");
-            appStart();
+                }));
+                console.log("-----------------------------------");
+                appStart();
+
+            });
         })
 
-    });
 
 }
 
