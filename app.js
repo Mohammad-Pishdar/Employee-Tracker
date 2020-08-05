6955ae00834676c0de2934705f847b0205fe08c2
Mo Pishdar <mohpish@gmail.com>
2020-08-06 09:39:53 +1000

    add function bug fixed so the show all function now can show newly addded employees

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const columnify = require('columnify');
-let employeeNames = [];
+let employeeNames = ["None", "John Doe", "Mike Chan", "Ashley Rodriguez", "Kevin Tupik", "Malia Brown", "Sarah Lourd", "Tom Allen", "Tammer Galal"];
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -16,7 +16,7 @@ connection.connect(err => {
     appStart();
 });
 
-appStart = () => {
+const appStart = () => {
     inquirer
         .prompt({
             name: "action",
@@ -85,6 +85,7 @@ appStart = () => {
 }
 
 const showAllEmployees = () => {
+    console.log(employeeNames);
     connection.query("SELECT employee.id, first_name, last_name, title, department, salary, manager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id", function (err, res) {
         for (let i = 0; i < res.length; i++) {
             for (let j = 0; j < res.length; j++) {
@@ -216,14 +217,8 @@ const addEmployee = () => {
     let department_id;
     let manager_id;
     let role_id;
-    connection.query("SELECT * FROM employee", function (err, res) {
-        if (err) throw err;
 
-        for (let i = 0; i < res.length; i++) {
-            employeeNames.push(res[i].first_name + " " + res[i].last_name);
-        }
-        employeeNames.push("None");
-        inquirer.prompt([{
+    inquirer.prompt([{
             name: "first_name",
             type: "input",
             message: "What is the employee's first name?"
@@ -250,14 +245,21 @@ const addEmployee = () => {
             type: "list",
             message: "Who is the employee's manager?",
             choices: employeeNames
-        }]).then(answer => {
+        }])
+        .then(answer => {
+
             role_id = employeeNames.length++;
 
             for (let i = 0; i < employeeNames.length; i++) {
                 if (employeeNames[i] === answer.manager) {
-                    manager_id = i + 1;
+                    manager_id = i;
+                }
+                if (employeeNames[i] === "None") {
+                    manager_id = null;
                 }
             }
+
+
             switch (answer.role) {
                 case "Sales Lead":
                     title = "Sales Lead";
@@ -338,8 +340,132 @@ const addEmployee = () => {
                 }
             );
         })
-    })
 }
+
+// connection.query("SELECT * FROM employee", function (err, res) {
+//     if (err) throw err;
+
+//     for (let i = 0; i < res.length; i++) {
+//         employeeNames.push(res[i].first_name + " " + res[i].last_name);
+//     }
+//     employeeNames.push("None");
+//     inquirer.prompt([{
+//         name: "first_name",
+//         type: "input",
+//         message: "What is the employee's first name?"
+//     }, {
+//         name: "last_name",
+//         type: "input",
+//         message: "What is the employee's last name?"
+//     }, {
+//         name: "role",
+//         type: "list",
+//         message: "What is the employee's role?",
+//         choices: [
+//             "Sales Lead",
+//             "Salesperson",
+//             "Lead Engineer",
+//             "Software Engineer",
+//             "Account Manager",
+//             "Accountant",
+//             "Legal Team Lead",
+//             "Lawyer"
+//         ]
+//     }, {
+//         name: "manager",
+//         type: "list",
+//         message: "Who is the employee's manager?",
+//         choices: employeeNames
+//     }]).then(answer => {
+//         role_id = employeeNames.length++;
+
+//         for (let i = 0; i < employeeNames.length; i++) {
+//             if (employeeNames[i] === answer.manager) {
+//                 manager_id = i + 1;
+//             }
+//         }
+//         switch (answer.role) {
+//             case "Sales Lead":
+//                 title = "Sales Lead";
+//                 salary = 100000;
+//                 department_id = 1;
+//                 department = "Sales";
+//                 break;
+
+//             case "Salesperson":
+//                 title = "Salesperson";
+//                 salary = 80000;
+//                 department_id = 1;
+//                 department = "Sales";
+//                 break;
+
+//             case "Lead Engineer":
+//                 title = "Lead Engineer";
+//                 salary = 150000;
+//                 department_id = 2;
+//                 department = "Engineering";
+//                 break;
+
+//             case "Software Engineer":
+//                 title = "Software Engineer";
+//                 salary = 120000;
+//                 department_id = 2;
+//                 department = "Engineering";
+//                 break;
+
+//             case "Account Manager":
+//                 title = "Account Manager";
+//                 salary = 140000;
+//                 department_id = 3;
+//                 department = "Finance";
+//                 break;
+
+//             case "Accountant":
+//                 title = "Accountant";
+//                 salary = 125000;
+//                 department_id = 3;
+//                 department = "Finance";
+//                 break;
+
+//             case "Legal Team Lead":
+//                 title = "Legal Team Lead";
+//                 salary = 250000;
+//                 department_id = 4;
+//                 department = "Legal";
+//                 break;
+
+//             case "Lawyer":
+//                 title = "Lawyer";
+//                 salary = 190000;
+//                 department_id = 4;
+//                 department = "Legal";
+//                 break;
+//         }
+//         connection.query(
+//             "INSERT INTO role SET ?", {
+//                 title: title,
+//                 salary: salary,
+//                 department_id: department_id,
+//             },
+//             err => {
+//                 if (err) throw err;
+//                 connection.query(
+//                     "INSERT INTO employee SET ?", {
+//                         first_name: answer.first_name,
+//                         last_name: answer.last_name,
+//                         role_id: role_id,
+//                         manager_id: manager_id
+//                     },
+//                     err => {
+//                         if (err) throw err;
+//                         appStart();
+//                     }
+//                 );
+//             }
+//         );
+//     })
+// })
+
 
 const removeEmployee = () => {
     connection.query("SELECT * FROM employee", function (err, res) {
